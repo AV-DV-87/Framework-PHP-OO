91857d396e3f50e84c55a14d4fc9e4451dc6b92d
Arnaud V <34190857+AV-DV-87@users.noreply.github.com>
2018-03-26 00:11:28 +0200

    Membre Controller
    Création du Membre Controller et des views

--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -1,7 +1,39 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Etudiant
- * Date: 23/03/2018
- * Time: 11:52
- */
+
+namespace Controller;
+
+class MembreController extends Controller
+{
+    //Récupère tous les membres
+    public function afficheAllM()
+    {
+        //1 Récupère tous les produits
+        $membres = $this->getModel()->getAllMember();
+
+        //3 Renvoyer la page de listing des membres
+
+        //solution sans render pour test
+        //require __DIR__ . '/../View/produit/boutique.php';
+
+        $params = array(
+            'membres'=>$membres
+        );
+
+        return $this -> render('layout.html', 'gestion_membre.html', $params);
+    }
+
+    //Affiche un membre
+    public function afficheM($id)
+    {
+        //1 recup les infos du produit dont l'id est $id
+        $membre = $this->getModel()->getMembreById($id);
+
+        //2 Renvoie la vue
+
+        $params = array(
+            'membre'=>$membre
+        );
+
+        return $this ->render('layout.html', 'fiche_membre.html', $params);
+    }
+}
--- src/Model/MembreModel.php
+++ src/Model/MembreModel.php
@@ -1,7 +1,34 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Etudiant
- * Date: 23/03/2018
- * Time: 10:12
- */
+
+namespace Model;
+
+use PDO; //pour les requêtes spécifiques à membres
+
+class MembreModel extends Model
+{
+    //tout le code de Model.php est ici
+
+    public function getAllMember() //renomme les fonctions de model pour être plus clair
+    {
+        return $this ->findAll();
+    }
+    public function getMemberById($id)
+    {
+        return $this -> find($id);
+    }
+    public function updateMember($id, $infos)
+    {
+        return $this->update($id, $infos);
+    }
+    public function deleteMember($id)
+    {
+        return $this ->delete($id);
+    }
+    public function registerMember($infos)
+    {
+        return $this->register($infos);
+    }
+    //fin des function héritées
+
+
+}
